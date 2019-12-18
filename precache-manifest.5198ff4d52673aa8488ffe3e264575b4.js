d116ab7413b33f39daacad8b7eb75fe04b9086ef
Ana Chernov <anastasia.chernov@gmail.com>
2019-12-18 14:40:29 -0500

    Updates

---
+++ precache-manifest.5198ff4d52673aa8488ffe3e264575b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10147277fce48f41bd8bc2e119a9a6ca",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "73fcd44aa49d1f5f18f3",
+    "url": "/Clicky-Game/static/css/main.292689f5.chunk.css"
+  },
+  {
+    "revision": "41a54356d8100bc9d2b6",
+    "url": "/Clicky-Game/static/js/2.8b160604.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Clicky-Game/static/js/2.8b160604.chunk.js.LICENSE"
+  },
+  {
+    "revision": "73fcd44aa49d1f5f18f3",
+    "url": "/Clicky-Game/static/js/main.3c66ef92.chunk.js"
+  },
+  {
+    "revision": "9686356c73b0faf68a98",
+    "url": "/Clicky-Game/static/js/runtime-main.b4ab0e47.js"
+  }
+]);
